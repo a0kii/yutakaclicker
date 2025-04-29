03943605ef2a6ec0102d29b3a4de0bde719a80f5
a0kii <60974327+a0kii@users.noreply.github.com>
2025-04-29 16:45:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let clickCount = 0;
     let lastClickTime = 0;
-    const minInterval = 100;
+    const minInterval = 200;
 
     const fullScreenWarning = document.createElement("div");
     fullScreenWarning.id = "click-warning";
