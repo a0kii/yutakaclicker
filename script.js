d2668c3e37a1857632be4d7d0bbfa041eee355d8
a0kii <60974327+a0kii@users.noreply.github.com>
2025-04-29 15:28:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const clickCountElement = document.getElementById("click-count");
+    const clickButton = document.getElementById("click-button");
+    const warningText = document.getElementById("warning");
+
+    if (window.Telegram && window.Telegram.WebApp) {
+        const userAgent = navigator.userAgent;
+        const isMobile = /iPhone|iPad|iPod|Android/i.test(userAgent);
+
+        if (!isMobile) {
+            clickButton.style.display = "none";
+            warningText.style.display = "block";
+        }
+    }
+
+    let clickCount = 0;
+    clickButton.addEventListener("click", () => {
+        clickCount++;
+        clickCountElement.textContent = clickCount;
+    });
+
+    const tg = window.Telegram.WebApp;
+
+    tg.expand();
+
+    tg.MainButton.setText("Отправить данные");
+    tg.MainButton.show();
+
+    tg.MainButton.onClick(() => {
+        const clickCount = document.getElementById("click-count").textContent;
+        
+        console.log("Отправка данных:", clickCount);
+
+        try {
+            tg.sendData(JSON.stringify({ clicks: clickCount }));
+            console.log("✅ Данные отправлены!");
+        } catch (error) {
+            console.error("❌ Ошибка отправки данных:", error);
+        }
+
+        tg.close();
+    });
+
+});
